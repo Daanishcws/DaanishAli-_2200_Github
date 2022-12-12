4ac83c6476936079baff2996bbcc36656af38188
Daanishcws <117249146+Daanishcws@users.noreply.github.com>
2022-12-11 23:57:48 -0600

    PE6.4

---
+++ DaanishAli_2200_PE6.4/src/integerreversed.java
@@ -0,0 +1,31 @@
+/***
+ * 
+ * @author daanish ali
+ *
+ */
+import java.util.Scanner;
+
+public class integerreversed {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in); 
+
+		System.out.println("Display and integer reversed:");
+		
+		
+		System.out.print("Enter an integer: ");
+		int number = input.nextInt();
+
+		
+		reverse(number);
+	}
+
+	
+	public static void reverse(int number) {
+		while (number > 0) {
+			System.out.print((number % 10));
+			number /= 10;
+		}
+		System.out.println();
+	}
+}
